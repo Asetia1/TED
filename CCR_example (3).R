e1c5105b6384611455625dd478a473eba135cb28
Asetia1 <113863496+Asetia1@users.noreply.github.com>
2025-11-12 15:02:36 -0500

    Add files via upload

---
+++ CCR_example (3).R
@@ -0,0 +1,16 @@
+install.packages("devtools")
+devtools::install_github("tomzhang255/CCR")
+library(CCR)
+ccr_setup()
+
+# agentic traits
+ccr_loadings_agentic <- ccr_wrapper(data_file = "/Users/aliahzewail/Downloads/tf.csv", 
+            data_col = "transcript", q_file = "/Users/aliahzewail/Downloads/agentic traits.xlsx - Sheet1.csv", q_col = "questiontext")
+
+write.csv(ccr_loadings_agentic, "ccr_agentic.csv")
+
+# communal traits
+ccr_loadings_communal <- ccr_wrapper(data_file = "/Users/aliahzewail/Downloads/tf.csv", 
+                            data_col = "transcript", q_file = "/Users/aliahzewail/Downloads/communal.xlsx - Sheet1.csv", q_col = "questiontext")
+
+write.csv(ccr_loadings_communal, "ccr_communal.csv")
